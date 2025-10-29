5a129032fdce8a63d6c8eea4370b8a7b093c725d
JoseH-s <josehribeirodossantos@gmail.com>
2025-10-29 17:43:11 -0300

    alteração app

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,14 @@ import Upload from './Components/Upload';
 import Form from './Components/Form';
 
 export function App() {
-  // controla qual tela mostrar
   const [isLogged, setIsLogged] = useState(false);
-  const [page, setPage] = useState('home'); // 'home' | 'upload'
+  const [page, setPage] = useState('home');
 
   return (
     <div className={`App ${!isLogged ? 'login-bg' : ''}`}>
       {!isLogged ? (
-        // Passa função para o Login avisar quando o login for feito
         <Login onLoginSuccess={() => setIsLogged(true)} />
       ) : (
-        // quando logado, decide qual componente interno mostrar
         page === 'home' ? (
           <Home onNavigate={(p) => setPage(p)} />
         ) : page === 'upload' ? (
