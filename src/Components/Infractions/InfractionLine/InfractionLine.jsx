622d175f29f9c019e9af3fc4e34c5193c7551306
João Pedro Torres <135067847+JoaoPBTorres@users.noreply.github.com>
2025-11-16 14:18:47 -0300

    Add files via upload

--- src/Components/Infractions/InfractionLine/InfractionLine.jsx
+++ src/Components/Infractions/InfractionLine/InfractionLine.jsx
@@ -7,16 +7,24 @@ export function InfractionLine({ dia, descricao, status, id, data }) {
     const navigate = useNavigate();
 
     const handleClick = () => {
-        navigate('/form', { state: { infraction: { id, dia, descricao, status, ...data } } });
+        navigate('/form', { 
+            state: { 
+                infraction: { 
+                    dbData: {id, dia, descricao, status, ...data } 
+                } 
+            } 
+        });
     };
+
+    const safeStatus = status ? status.toLowerCase() : "default";
 
     return (
         <tr className={styles.row} onClick={handleClick}>
             <td>{dia}</td>
             <td>{descricao}</td>
             <td>
-                <div className={`${styles.statusIndicator} ${styles[status.toLowerCase()]}`}
-                    title={status}>
+                <div className={`${styles.statusIndicator} ${styles[safeStatus]}`}
+                    title={status ?? "Sem status"}>
                 </div>
             </td>
         </tr>
@@ -26,7 +34,7 @@ export function InfractionLine({ dia, descricao, status, id, data }) {
 InfractionLine.propTypes = {
     dia: PropTypes.string.isRequired,
     descricao: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
+    status: PropTypes.string,
     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     data: PropTypes.object,
 }
